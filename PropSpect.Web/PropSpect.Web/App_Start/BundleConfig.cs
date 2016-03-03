50ef1a2b25c55e3fe8efd95d9a0faaeaf3cbcbfc
Eckardt Briedenhann <maxchal72@gmail.com>
2016-03-03 19:16:28 +0200

    Removed bootstrap, added materializecss. Design on layout. Design on properties

--- PropSpect.Web/PropSpect.Web/App_Start/BundleConfig.cs
+++ PropSpect.Web/PropSpect.Web/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace PropSpect.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                      "~/Content/materialize-v0.97.1/js/materialize.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/materialize-v0.97.1/css/materialize.css",
                       "~/Content/site.css"));
         }
     }
