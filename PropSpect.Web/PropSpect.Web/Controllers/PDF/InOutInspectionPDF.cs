905e5500793b5fdb8e128dccee2bc95a8e5d0764
Eckardt Briedenhann <maxchal72@gmail.com>
2016-09-22 21:54:04 +0200

    Created a base iteration of the PDF report generator,

---
+++ PropSpect.Web/PropSpect.Web/Controllers/PDF/InOutInspectionPDF.cs
@@ -0,0 +1,102 @@
+﻿using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PropSpect.Web.Controllers.PDF
+{
+    public class InOutInspectionPDF
+    {
+        private PDFManager p;
+        private double currentLine;
+        private double currentWidth;
+        private string currentSection;
+
+        public InOutInspectionPDF(PDFManager manager)
+        {
+            p = manager;
+        }
+
+
+        public void GeneratePDF()
+        {
+            // Create New PDF
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "In-Out Inspection Report";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+            page.Size = PageSize.A4;
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            //Create Header and set current height
+            DrawHeaderAndFooter(document, page, gfx, "In-Out Inspection");
+            currentLine = 80;
+            currentWidth = 20;
+
+            //Inspection Detais
+            currentSection = "Inspection Details";
+            
+            p.GenerateHeadingBlock(currentSection,page, gfx, currentWidth, currentLine);
+            
+
+            // Create a font
+            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+
+            // Draw the text            
+            gfx.DrawString("In-Out Inspection", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height),
+            XStringFormats.Center);
+
+            p.Save(document);
+        }
+
+        public void DrawHeaderAndFooter(PdfDocument document, PdfPage page, XGraphics gfx, string title)
+        {
+            //PropSpect Logo
+            var filename = Path.Combine(p.ServerProperty.MapPath("~/Content/Images"), "PropSpect.png");
+            XImage image = XImage.FromFile(filename);           
+            gfx.DrawImage(image, 20, 20, 120, 50);
+
+
+            //Title Text
+            XRect rect = new XRect(new XPoint(), gfx.PageSize);
+            rect.Inflate(-10, -25);
+            gfx.DrawString(title, p.Font16Bold, XBrushes.Black, rect, XStringFormats.TopCenter);
+
+            //Title Date
+            rect.Inflate(0,-18);
+            gfx.DrawString("Date: " + DateTime.Now.ToString("yyyy-MM-dd"), p.Font14, XBrushes.Black, rect, XStringFormats.TopCenter);
+
+            //Customer Logo
+            filename = Path.Combine(p.ServerProperty.MapPath("~/Content/Images"), "olea.png");
+            image = XImage.FromFile(filename);
+            gfx.DrawImage(image, page.Width-20-120, 20, 120, 50);
+
+
+            //Footer
+
+
+
+
+            
+            rect.Offset(0, 5);
+            XStringFormat format = new XStringFormat();
+            format.Alignment = XStringAlignment.Near;
+            format.LineAlignment = XLineAlignment.Far;
+            gfx.DrawString("Copyright: Paideia Solutions 2016", p.Font8Italic, XBrushes.Black, rect, format);
+
+            
+            format.Alignment = XStringAlignment.Far;
+            gfx.DrawString(document.PageCount.ToString(), p.Font8Italic, XBrushes.Black, rect, format);
+
+            document.Outlines.Add(title, page, true);
+
+        }
+    }
+}
---
+++ PropSpect.Web/PropSpect.Web/Controllers/PDF/PDFManager.cs
@@ -0,0 +1,116 @@
+﻿using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PropSpect.Web.Controllers.PDF
+{
+    public class PDFManager : Controller
+    {
+        //Pens
+        public XPen PropspectPen { get; set; }
+
+        //Brushes
+        public XSolidBrush PropspectBrush { get; set; }
+
+        //Fonts
+        public XFont Font8 { get; set; }
+        public XFont Font10 { get; set; }
+        public XFont Font12 { get; set; }        
+        public XFont Font14 { get; set; }       
+        public XFont Font16 { get; set; }
+        public XFont Font18 { get; set; }
+        public XFont Font8Bold { get; set; }
+        public XFont Font10Bold { get; set; }
+        public XFont Font12Bold { get; set; }
+        public XFont Font14Bold { get; set; }
+        public XFont Font16Bold { get; set; }
+        public XFont Font18Bold { get; set; }
+        public XFont Font8Italic { get; set; }
+        public XFont Font10Italic { get; set; }
+        public XFont Font12Italic { get; set; }
+        public XFont Font14Italic { get; set; }
+        public XFont Font16Italic { get; set; }
+        public XFont Font18Italic { get; set; }
+
+
+
+        public HttpServerUtilityBase ServerProperty { get; set; }
+
+        public PDFManager(HttpServerUtilityBase server)
+        {
+            ServerProperty = server;
+
+            //Brushes
+            InitializeBrushes();
+
+            //Pens
+            InitializePens();
+
+            //Fonts
+            InitializeFonts("Calibri");
+        }
+    
+
+        public void Save (PdfDocument document)
+        {
+            // Save the document...  
+            var filename = Path.Combine(ServerProperty.MapPath("~/Resources/PDF"), "test.pdf");
+            document.Save(filename);
+
+        }
+
+        private void InitializeBrushes()
+        {
+            PropspectBrush = new XSolidBrush(XColor.FromCmyk(0.5424, 0.0508, 0, 0.3059));
+
+        }
+
+        public void InitializePens()
+        {
+            PropspectPen = new XPen(XColor.FromCmyk(0.5424, 0.0508, 0, 0.3059));
+        }
+
+        public void InitializeFonts(string font)
+        {
+            //Regular
+            Font8 = new XFont(font, 8, XFontStyle.Regular);
+            Font10 = new XFont(font, 10, XFontStyle.Regular);
+            Font12 = new XFont(font, 12, XFontStyle.Regular);
+            Font14 = new XFont(font, 14, XFontStyle.Regular);
+            Font16 = new XFont(font, 16, XFontStyle.Regular);
+            Font18 = new XFont(font, 18, XFontStyle.Regular);
+
+            //Bold
+            Font8Bold = new XFont(font, 8, XFontStyle.Bold);
+            Font10Bold = new XFont(font, 10, XFontStyle.Bold);
+            Font12Bold = new XFont(font, 12, XFontStyle.Bold);
+            Font14Bold = new XFont(font, 14, XFontStyle.Bold);
+            Font16Bold = new XFont(font, 16, XFontStyle.Bold);
+            Font18Bold = new XFont(font, 18, XFontStyle.Bold);
+
+            //Italic
+            Font8Italic = new XFont(font, 8, XFontStyle.Italic);
+            Font10Italic = new XFont(font, 10, XFontStyle.Italic);
+            Font12Italic = new XFont(font, 12, XFontStyle.Italic);
+            Font14Italic = new XFont(font, 14, XFontStyle.Italic);
+            Font16Italic = new XFont(font, 16, XFontStyle.Italic);
+            Font18Italic = new XFont(font, 18, XFontStyle.Italic);
+        }
+
+        public void GenerateHeadingBlock(string title, PdfPage page, XGraphics gfx, double currentWidth, double currentLine)
+        {
+            XRect headerRect = new XRect(currentWidth, currentLine, page.Width - currentWidth * 2, 17);
+            gfx.DrawRectangle(PropspectBrush, currentWidth, currentLine, page.Width - currentWidth * 2, 18);
+            gfx.DrawString(title, Font16Bold, XBrushes.White, headerRect, XStringFormats.Center);
+        }
+        
+
+    }
+}
